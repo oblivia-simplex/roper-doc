f66e2ffbf1c6c6840ec78ced9badd0034edf53e9
oblivia simplex <oblivia@paranoici.org>
2018-04-17 13:16:33 -0300

    last hurrah

--- slides/mcs_def/auto/mcs_def_slides.el
+++ slides/mcs_def/auto/mcs_def_slides.el
@@ -6,15 +6,15 @@
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -34,53 +34,57 @@
     "hyperref"
     "listings")
    (LaTeX-add-labels
-    "sec:org676fa7a"
-    "sec:org3d7dd21"
-    "sec:org2204cb8"
-    "sec:org41207fe"
-    "sec:orgfb842f7"
-    "sec:orgcbe1102"
-    "sec:org7572833"
-    "sec:org35f607f"
-    "sec:orge062217"
-    "sec:org049a52b"
-    "sec:orgdb7034d"
-    "sec:orgda05ca4"
-    "sec:orgbfd030e"
-    "sec:org4830365"
-    "sec:orge4173d8"
-    "sec:org902e5c6"
-    "sec:org47b09b6"
-    "sec:orge0375ee"
-    "sec:orgd197e29"
-    "sec:orgd176949"
-    "sec:org9c68dee"
-    "sec:orgb7bf510"
-    "sec:org2872d90"
-    "sec:orgecd27af"
-    "sec:orgdabd3a3"
-    "sec:org2c2eb9a"
-    "sec:org0f659e5"
-    "sec:orgca90f92"
-    "sec:orge6232b0"
-    "sec:orgbdd19c1"
-    "sec:org77df97e"
-    "sec:orgc50dfda"
-    "sec:orgb851e23"
-    "sec:org406b458"
-    "sec:orgb879aa5"
-    "sec:orgebdda3b"
-    "sec:orge8c6789"
-    "sec:orgc465b14"
-    "sec:orga32e9d7"
-    "sec:orgaca287e"
-    "sec:org9ce225f"
-    "sec:orgde4f25c"
-    "sec:orgb3881c7"
-    "sec:org12798ba"
-    "sec:org94c9d7d"
-    "sec:orgce98f5d"
-    "sec:org19e58e3"
-    "sec:orgb8f9945"))
+    "sec:org129b2b2"
+    "sec:org741ce42"
+    "sec:org7692954"
+    "sec:org4262e0d"
+    "sec:org86a7968"
+    "sec:org68df840"
+    "sec:org1891993"
+    "sec:org6153d9a"
+    "sec:org833f80b"
+    "sec:org7758608"
+    "sec:org4ad1c46"
+    "sec:org41c4d6f"
+    "sec:orgdc07e33"
+    "sec:orgcdb6f68"
+    "sec:org994ecc7"
+    "sec:orgbcd6419"
+    "sec:org9c9a48a"
+    "sec:org4832823"
+    "sec:orgc2816ef"
+    "sec:org6692595"
+    "sec:org60c724a"
+    "sec:orgd83c29b"
+    "sec:org0272985"
+    "sec:org0dbc1fd"
+    "sec:orga2ccf63"
+    "sec:orgcc6d791"
+    "sec:org7f7f071"
+    "sec:org72ab662"
+    "sec:orgb54fa93"
+    "sec:orge812295"
+    "sec:org58529ca"
+    "sec:orgba2d711"
+    "sec:org7823ca1"
+    "sec:orgf7fc28b"
+    "sec:orgf0d6f53"
+    "sec:org26e41bc"
+    "sec:orgf30e301"
+    "sec:org648b83c"
+    "sec:orgac97367"
+    "sec:org828ab61"
+    "sec:orga37a17e"
+    "sec:orge477a57"
+    "sec:org1c7ef2e"
+    "sec:orge864319"
+    "sec:org993ec3e"
+    "sec:org9a9ebff"
+    "sec:orgb1565c3"
+    "sec:orged5c1ba"
+    "sec:org3a5e1a4"
+    "sec:org38a07a3"
+    "sec:org400cfb2"
+    "sec:org9e6531d"))
  :latex)
 
